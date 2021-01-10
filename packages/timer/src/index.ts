2249c289eb69e07622af491a0a8b58bcb9ed6859
Canale <raffaelecanale@gmail.com>
2021-01-10 11:42:24 +0100

    Bugfix

--- packages/timer/src/index.ts
+++ packages/timer/src/index.ts
@@ -1,2 +1,4 @@
 export { default as ReusableTimeout } from './ReusableTimeout';
+export { default as ReusableInterval } from './ReusableInterval';
+export { default as ReusableTimer } from './ReusableTimer';
 export { days, hours, minutes, seconds } from './Utils';
